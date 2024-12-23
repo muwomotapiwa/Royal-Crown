a16fc61a5ff21d32f454f2d09a2b3a89ab8d03d3
muwomotapiwa <muwomotapiwa@gmail.com>
2024-12-23 06:52:31 +0200

    js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
-function toggleMenu() {
-    const mobileMenu = document.getElementById('mobile-menu');
-    const isDisplayed = mobileMenu.style.display === 'flex';
-    mobileMenu.style.display = isDisplayed ? 'none' : 'flex';
-}
+const menuIcon = document.getElementById('menuIcon');
+const mobileNav = document.getElementById('mobileNav');
 
-// Close the menu when a link is clicked
-document.querySelectorAll('.mobile-menu a').forEach(link => {
-    link.addEventListener('click', () => {
-        document.getElementById('mobile-menu').style.display = 'none';
-    });
+menuIcon.addEventListener('click', () => {
+    mobileNav.classList.toggle('active');
 });
