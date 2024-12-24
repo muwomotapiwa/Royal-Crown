cc94370394afb55967f64d29d5d618058b31f62d
muwomotapiwa <muwomotapiwa@gmail.com>
2024-12-24 12:39:57 +0200

    new

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+function openModal(videoSrc) {
+    const modal = document.getElementById('videoModal');
+    const video = document.getElementById('sermonVideo');
+    video.src = videoSrc;
+    modal.style.display = 'flex';
+    video.play();
+}
+
+function closeModal(event) {
+    const modal = document.getElementById('videoModal');
+    const video = document.getElementById('sermonVideo');
+    video.pause();
+    video.src = "";
+    modal.style.display = 'none';
+}
+
+function toggleMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('show');
+}
