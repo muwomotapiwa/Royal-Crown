d566a2a09562b2d11d2718410e391595fea9277e
muwomotapiwa <muwomotapiwa@gmail.com>
2025-03-22 18:00:17 +0200

    dadasd

--- js/contact.js
+++ js/contact.js
@@ -19,22 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-    // Contact Form Submission
-    const form = document.getElementById("contactForm");
-    form.addEventListener("submit", (e) => {
-        e.preventDefault(); // Prevent form submission
-
-        // Collect form data
-        const name = document.getElementById("name").value;
-        const email = document.getElementById("email").value;
-        const message = document.getElementById("message").value;
-
-        // Simple feedback
-        alert(`Thank you, ${name}! Your message has been sent.`);
-
-        // Reset the form
-        form.reset();
-    });
+    
 
     // Hamburger Menu Functionality
     const hamburger = document.querySelector(".hamburger");
